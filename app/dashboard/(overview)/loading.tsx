9e402b693a4e924f9a3e8658001e6392e98f96db
wmb0412 <353685720@qq.com>
2023-12-15 13:15:12 +0800

    feat: learn next chapter 6 - 16

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import DashboardSkeleton from '@/app/ui/skeletons';
+
+export default function Loading() {
+  return (
+    <DashboardSkeleton />
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next';
 import "@/app/ui/global.css"
 export default function RootLayout({
   children,
@@ -10,3 +11,11 @@ export default function RootLayout({
     </html>
   );
 }
+
+
+ 
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
